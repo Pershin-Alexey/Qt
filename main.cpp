bf20392b2125df55554b53e575514ee090128457
Pershin-Alexey <bazuluk@rambler.ru>
2024-06-25 12:56:38 +0400

    m1

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 int main(int argc, char *argv[])
 {
     /* что-то добавили
-    давай еще что нибудь добавим */
+    давай еще что нибудь добавим
+    еще для примера*/
 
     QApplication a(argc, argv);
     MainWindow w;
